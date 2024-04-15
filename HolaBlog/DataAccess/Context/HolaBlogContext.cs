72250cc21cd3643a9c28d02cc7380bb1092c04d9
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-16 01:58:23 +0300

    + Context Class Created

---
+++ HolaBlog/DataAccess/Context/HolaBlogContext.cs
@@ -0,0 +1,23 @@
+﻿using HolaBlog.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HolaBlog.DataAccess.Context
+{
+    public class HolaBlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=HolaBlogDB;Integrated Security=True");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
