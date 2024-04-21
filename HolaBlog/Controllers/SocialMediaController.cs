c0b617d578546a25cdb5ff3acc7549e85bf73960
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-21 22:07:56 +0300

    + Testimonial and SocialMedia CRUD Operation Coded

---
+++ HolaBlog/Controllers/SocialMediaController.cs
@@ -0,0 +1,57 @@
+﻿using HolaBlog.DataAccess.Context;
+using HolaBlog.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaBlog.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public SocialMediaController(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
+        public IActionResult SocialMediaList()
+        {
+            var result = _holaBlogContext.SocialMedias.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSocialMedia(SocialMedia socialMedia)
+        {
+            _holaBlogContext.SocialMedias.Add(socialMedia);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = _holaBlogContext.SocialMedias.Find(id);
+            _holaBlogContext.SocialMedias.Remove(value);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+            var value = _holaBlogContext.SocialMedias.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            _holaBlogContext.SocialMedias.Update(socialMedia);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+    }
+}
---
+++ HolaBlog/Controllers/TestimonialController.cs
@@ -0,0 +1,57 @@
+﻿using HolaBlog.DataAccess.Context;
+using HolaBlog.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaBlog.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public TestimonialController(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
+        public IActionResult TestimonialList()
+        {
+            var result = _holaBlogContext.Testimonials.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(Testimonial testimonial)
+        {
+            _holaBlogContext.Testimonials.Add(testimonial);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = _holaBlogContext.Testimonials.Find(id);
+            _holaBlogContext.Testimonials.Remove(value);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateTestimonial(int id)
+        {
+            var value = _holaBlogContext.Testimonials.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            _holaBlogContext.Testimonials.Update(testimonial);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+    }
+}
