d65384ff93e26c8009f516c547c2c10774207ed6
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-22 01:35:18 +0300

    + fixed

--- HolaBlog/ViewComponents/LayoutViewComponent/LayoutNavbarComponentPartial.cs
+++ HolaBlog/ViewComponents/LayoutViewComponent/LayoutNavbarComponentPartial.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HolaBlog.DataAccess.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HolaBlog.ViewComponents.LayoutViewComponent
 {
     public class LayoutNavbarComponentPartial : ViewComponent
     {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public LayoutNavbarComponentPartial(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            ViewBag.toDoListCount = _holaBlogContext.ToDoLists.Where(x => x.Status == false).Count();
+            var values = _holaBlogContext.ToDoLists.Where(x => x.Status == false).ToList();
+            return View(values);
         }
     }
 }
