f942140f3d554c2134393c547c96fe3e4b39e14f
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-21 21:25:35 +0300

    + Skill and Portfolio CRUD Operation Coded

---
+++ HolaBlog/Controllers/PortfolioController.cs
@@ -0,0 +1,58 @@
+﻿using HolaBlog.DataAccess.Context;
+using HolaBlog.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaBlog.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public PortfolioController(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
+        public IActionResult PortfolioList()
+        {
+            var result = _holaBlogContext.Portfolios.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult CreatePortfolio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePortfolio(Portfolio portfolio)
+        {
+            _holaBlogContext.Portfolios.Add(portfolio);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var value = _holaBlogContext.Portfolios.Find(id);
+            _holaBlogContext.Portfolios.Remove(value);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var value = _holaBlogContext.Portfolios.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            _holaBlogContext.Portfolios.Update(portfolio);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+    }
+}
+
---
+++ HolaBlog/Controllers/SkillController.cs
@@ -0,0 +1,57 @@
+﻿using HolaBlog.DataAccess.Context;
+using HolaBlog.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaBlog.Controllers
+{
+    public class SkillController : Controller
+    {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public SkillController(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
+        public IActionResult SkillList()
+        {
+            var result = _holaBlogContext.Skills.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSkill(Skill skill)
+        {
+            _holaBlogContext.Skills.Add(skill);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var value = _holaBlogContext.Skills.Find(id);
+            _holaBlogContext.Skills.Remove(value);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSkill(int id)
+        {
+            var value = _holaBlogContext.Skills.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSkill(Skill skill)
+        {
+            _holaBlogContext.Skills.Update(skill);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+    }
+}
