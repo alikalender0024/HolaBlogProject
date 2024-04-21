62a7c27600d33e796ca256c53c3be39a2082e6e2
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-21 23:49:30 +0300

    + Message and Istatistic Coded

--- HolaBlog/Controllers/MessageController.cs
+++ HolaBlog/Controllers/MessageController.cs
@@ -1,12 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HolaBlog.DataAccess.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HolaBlog.Controllers
 {
     public class MessageController : Controller
     {
-        public IActionResult Index()
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public MessageController(HolaBlogContext holaBlogContext)
         {
-            return View();
+            _holaBlogContext = holaBlogContext;
+        }
+        public IActionResult Inbox()
+        {
+            var result = _holaBlogContext.Messages.ToList();
+            return View(result);
+        }
+        public IActionResult ChangeIsReadToTrue(int id)
+        {
+            var result = _holaBlogContext.Messages.Find(id);
+            result.IsRead=true;
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult ChangeIsReadToFalse(int id)
+        {
+            var result = _holaBlogContext.Messages.Find(id);
+            result.IsRead=false;
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult DeleteMessage(int id)
+        {
+            var result = _holaBlogContext.Messages.Find(id);
+            _holaBlogContext.Remove(result);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult MessageDetail(int id)
+        {
+            var result = _holaBlogContext.Messages.Find(id);
+            return View(result);
         }
     }
 }
---
+++ HolaBlog/Controllers/StatisticController.cs
@@ -0,0 +1,23 @@
+﻿using HolaBlog.DataAccess.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaBlog.Controllers
+{
+	public class StatisticController : Controller
+	{
+		private readonly HolaBlogContext _holaBlogContext;
+
+		public StatisticController(HolaBlogContext holaBlogContext)
+		{
+			_holaBlogContext = holaBlogContext;
+		}
+		public IActionResult Index()
+		{
+			ViewBag.testimonialCount = _holaBlogContext.Testimonials.ToList().Count;
+			ViewBag.skilllCount = _holaBlogContext.Skills.ToList().Count;
+			ViewBag.blogCount = _holaBlogContext.Blogs.ToList().Count;
+			ViewBag.isReadMessageCount = _holaBlogContext.Messages.ToList().Where(x => x.IsRead==false).Count();
+			return View();
+		}
+	}
+}
