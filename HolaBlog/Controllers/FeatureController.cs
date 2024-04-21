a0cb3fe09a2a4f0c8bd7d8ab7b71f19ceaa5b595
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-21 20:58:55 +0300

    + Feature Entities CRUD Operation Coded

---
+++ HolaBlog/Controllers/FeatureController.cs
@@ -0,0 +1,57 @@
+﻿using HolaBlog.DataAccess.Context;
+using HolaBlog.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaBlog.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public FeatureController(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
+        public IActionResult FeatureList()
+        {
+            var result = _holaBlogContext.Features.ToList();
+            return View(result);
+        }
+        [HttpGet]
+        public IActionResult CreateFeature()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(Feature feature)
+        {
+            _holaBlogContext.Features.Add(feature);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("FeatureList");
+        }
+
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _holaBlogContext.Features.Find(id);
+            _holaBlogContext.Features.Remove(value);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("FeatureList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateFeature(int id)
+        {
+            var value = _holaBlogContext.Features.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateFeature(Feature feature)
+        {
+            _holaBlogContext.Features.Update(feature);
+            _holaBlogContext.SaveChanges();
+            return RedirectToAction("FeatureList");
+        }
+    }
+}
