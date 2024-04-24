4ae8f6db4a70516e2f548ea81aecdc65fc19cddb
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-04-24 12:36:11 +0300

    + Edited

--- HolaBlog/ViewComponents/FooterComponentPartial.cs
+++ HolaBlog/ViewComponents/FooterComponentPartial.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HolaBlog.DataAccess.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HolaBlog.ViewComponents
 {
     public class FooterComponentPartial : ViewComponent
     {
+        private readonly HolaBlogContext _holaBlogContext;
+
+        public FooterComponentPartial(HolaBlogContext holaBlogContext)
+        {
+            _holaBlogContext= holaBlogContext;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();
+            var result = _holaBlogContext.SocialMedias.ToList();
+            return View(result);
         }
     }
 }
